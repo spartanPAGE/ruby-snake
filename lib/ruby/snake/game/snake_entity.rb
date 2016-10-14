db0b08e1d6ea04f2c097b4feeaef3a3bb09b5fb8
spartanPAGE <spartanpageagain@gmail.com>
2016-10-14 20:44:14 +0200

    Changed SnakeEntity to snake head (which also loads head resource)

--- lib/ruby/snake/game/snake_entity.rb
+++ lib/ruby/snake/game/snake_entity.rb
@@ -1,6 +1,16 @@
+require 'gosu'
+require 'ruby/helper/resources/loader'
+require 'ruby/snake/game/entity'
+
+
 module Ruby
   module Snake
-    class SnakeEntity
+    class SnakeHead < Game::Entity
+      @@head = Helper::Resource.load('res/snake-head.bmp')
+
+      def draw
+        @@head.draw_rot(@pos_x, @pos_y, @z_order, @rotation)
+      end
     end
   end
 end
