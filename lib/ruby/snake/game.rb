e08c88a49dcbc238a5cc9c9a33491fcc3d893075
spartanPAGE <spartanpageagain@gmail.com>
2016-10-14 18:14:55 +0200

    Added GameApp class

---
+++ lib/ruby/snake/game.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'gosu'
+
+require 'ruby/snake/game/window'
+
+module Ruby
+  module Snake
+
+    class GameApp
+      def initialize
+        @game_window = Game::Window.new
+      end
+
+      def start
+        @game_window.show
+      end
+    end
+
+  end
+end
