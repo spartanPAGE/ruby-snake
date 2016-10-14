12edceb370fe5ad853c92dcd3d2af2f475455d88
spartanPAGE <spartanpageagain@gmail.com>
2016-10-14 21:58:24 +0200

    SnakeHead changes it's velocity based on speed and angle

--- lib/ruby/snake/game/snake_entity.rb
+++ lib/ruby/snake/game/snake_entity.rb
@@ -6,7 +6,21 @@ require 'ruby/snake/game/entity'
 module Ruby
   module Snake
     class SnakeHead < Game::Entity
+      attr_accessor :speed
+
       @@head = Helper::Resource.load('res/snake-head.bmp')
+
+
+      def initialize(pos_x: 0, pos_y: 0, angle: 0.0, speed: 0.0)
+        @speed = speed
+        super(pos_x: pos_x, pos_y: pos_y, rotation: angle)
+      end
+
+      def update
+        @vel_x = Gosu::offset_x(@rotation, @speed)
+        @vel_y = Gosu::offset_y(@rotation, @speed)
+        super
+      end
 
       def draw
         @@head.draw_rot(@pos_x, @pos_y, @z_order, @rotation)
