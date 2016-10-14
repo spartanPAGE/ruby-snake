bd0f02f7c3ae707b14feb714f5b757e950060ea3
spartanPAGE <spartanpageagain@gmail.com>
2016-10-14 19:18:33 +0200

    removed generated test

--- spec/ruby/snake_spec.rb
+++ spec/ruby/snake_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
+require 'ruby/snake'
 
 describe Ruby::Snake do
   it 'has a version number' do
     expect(Ruby::Snake::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
