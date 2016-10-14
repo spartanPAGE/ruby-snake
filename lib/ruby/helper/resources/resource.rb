c3d38db700c0a32d669019935d6a92c7a7e4eeab
spartanPAGE <spartanpageagain@gmail.com>
2016-10-14 19:19:13 +0200

    Added resource module

---
+++ lib/ruby/helper/resources/resource.rb
@@ -0,0 +1,10 @@
+module Ruby
+  module Helper
+    module Resource
+      def self.validate_ext(path, avail_exts)
+        valid_exts_str = avail_exts.join('|')
+        path =~ /.*\.[#{valid_exts_str}]/
+      end
+    end
+  end
+end
