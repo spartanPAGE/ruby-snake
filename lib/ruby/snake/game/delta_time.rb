b42fd37d517895edbcb8478614d93a0153f00922
spartanPAGE <spartanpageagain@gmail.com>
2016-10-14 23:32:21 +0200

    fixed delta_time

--- lib/ruby/snake/game/delta_time.rb
+++ lib/ruby/snake/game/delta_time.rb
@@ -1,10 +1,16 @@
-require 'gosu'
-
 module Ruby
   module Snake
     module Game
+      @@actual_time = 0
+      @@last_time = 0
+
       def self.delta_time
-        return Gosu::milliseconds/1000
+        @@actual_time - @@last_time
+      end
+
+      def self.update_delta_time(milis)
+        @@last_time = @@actual_time
+        @@actual_time = milis
       end
     end
   end
