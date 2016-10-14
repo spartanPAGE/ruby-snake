b260aa5deda8a2c86ae7273d1010bfa649cbec70
spartanPAGE <spartanpageagain@gmail.com>
2016-10-14 19:23:54 +0200

    added more test cases for validate_ext module method

--- spec/ruby/resource_spec.rb
+++ spec/ruby/resource_spec.rb
@@ -3,8 +3,17 @@ require 'ruby/helper/resources/resource'
 
 describe Ruby::Helper::Resource do
   validate_ext = -> (path, avail_exts) { Ruby::Helper::Resource.validate_ext(path, avail_exts) }
-  it 'validates file ext correctly' do
-    expect(validate_ext.('test.png', ['png'])).not_to be nil
-  end
+  context 'validate_ext' do
+    it 'handles proper exts well' do
+      expect(validate_ext.('test.png', ['png'])).not_to be nil
+    end
 
+    it 'handles bad exts correctly' do
+      expect(validate_ext.('', ['jpg'])).to be nil
+    end
+
+    it 'handles many exts' do
+      expect(validate_ext.('test.jpg', ['jpg', 'something_else'])).not_to be nil
+    end
+  end
 end
