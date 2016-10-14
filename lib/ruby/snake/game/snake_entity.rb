2c4e9114054ccbabaac4b88e8b746d805065fff1
spartanPAGE <spartanpageagain@gmail.com>
2016-10-14 22:07:36 +0200

    added some fun to the snake's head

--- lib/ruby/snake/game/snake_entity.rb
+++ lib/ruby/snake/game/snake_entity.rb
@@ -7,16 +7,51 @@ module Ruby
   module Snake
     class SnakeHead < Game::Entity
       attr_accessor :speed
+      attr_accessor :torsion_angle
 
       @@head = Helper::Resource.load('res/snake-head.bmp')
 
 
-      def initialize(pos_x: 0, pos_y: 0, angle: 0.0, speed: 0.0)
+      def initialize(pos_x: 0, pos_y: 0, angle: 0.0, speed: 0.0, torsion_angle: 0.0)
         @speed = speed
+        @torsion_angle = torsion_angle
         super(pos_x: pos_x, pos_y: pos_y, rotation: angle)
       end
 
+      def turn_left
+        @rotation -= @torsion_angle
+      end
+
+      def turn_right
+        @rotation += @torsion_angle
+      end
+
+      def accelerate
+        @speed += 0.1
+
+      end
+
+      def decelerate
+        @speed -= 0.1
+        if @speed < 0
+          @speed = 0
+        end
+      end
+
       def update
+        if Gosu::button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft then
+          turn_left
+        end
+        if Gosu::button_down? Gosu::KbRight or Gosu::button_down? Gosu::GpRight then
+          turn_right
+        end
+        if Gosu::button_down? Gosu::KbUp or Gosu::button_down? Gosu::GpUp then
+          accelerate
+        end
+        if Gosu::button_down? Gosu::KbDown or Gosu::button_down? Gosu::GpDown then
+          decelerate
+        end
+
         @vel_x = Gosu::offset_x(@rotation, @speed)
         @vel_y = Gosu::offset_y(@rotation, @speed)
         super
--- lib/ruby/snake/game/window.rb
+++ lib/ruby/snake/game/window.rb
@@ -6,9 +6,9 @@ module Ruby
     module Game
       class Window < Gosu::Window
         def initialize
-          super(640, 480)
+          super(640*2, 480*2)
           self.caption = "Snake game"
-          @snake = SnakeHead.new(pos_x: 100, pos_y: 100, speed: 0.1, angle: 2)
+          @snake = SnakeHead.new(pos_x: 100, pos_y: 100, speed: 0.1, angle: 2, torsion_angle: 4.5)
         end
 
         def update
