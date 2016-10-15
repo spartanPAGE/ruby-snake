26ef6054b77bbf06db68a910d9a42f8f108770b4
spartanPAGE <spartanpageagain@gmail.com>
2016-10-15 12:37:11 +0200

    added AngleBasedEntity spec skeleton; Todo: test angle based movement

---
+++ spec/ruby/angle_based_entity_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'ruby/snake/game/angle_based_entity'
+
+describe Ruby::Snake::Game::AngleBasedEntity do
+  AngleBasedEntity = Ruby::Snake::Game::AngleBasedEntity
+
+  context 'is created properly' do
+    let(:entity) { AngleBasedEntity.new(pos_x: 10, pos_y: 15, angle: 5, speed: 1) }
+    it 'has proper position' do
+      expect(entity.pos_x).to eq(10)
+      expect(entity.pos_y).to eq(15)
+    end
+
+    it 'has proper rotation' do
+      expect(entity.rotation).to eq(5)
+    end
+
+    it 'has proper speed' do
+      expect(entity.speed).to eq(1)
+    end
+  end
+end
