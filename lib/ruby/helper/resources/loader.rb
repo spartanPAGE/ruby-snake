c057ee8b44bfcb0e0713204c6551c5b50d898c8e
Patryk Wertka <spartanpageagain@gmail.com>
2016-10-17 08:50:59 +0200

    added sound support for Resource helper

--- lib/ruby/helper/resources/loader.rb
+++ lib/ruby/helper/resources/loader.rb
@@ -6,20 +6,27 @@ module Ruby
     # Contains resource's loading
     module Resource
       AVAILABLE_IMAGE_EXTS = %w(bmp jpg jpeg png).freeze
-      def self.load(path)
-        load_image path
-      end
+      AVAILABLE_SOUND_EXTS = %w(wav).freeze
 
-      def self.load_image(path)
-        create_image path if valid_img_ext? path
+      def self.load(path)
+        return create_image path if valid_img_ext? path
+        return create_sound path if valid_sound_ext? path
       end
 
       private_class_method def self.valid_img_ext?(path)
         validate_ext(path, AVAILABLE_IMAGE_EXTS)
       end
 
+      private_class_method def self.valid_sound_ext?(path)
+        validate_ext(path, AVAILABLE_SOUND_EXTS)
+      end
+
       private_class_method def self.create_image(path)
         Gosu::Image.new path
+      end
+
+      private_class_method def self.create_sound(path)
+        Gosu::Sample.new path
       end
     end
   end
