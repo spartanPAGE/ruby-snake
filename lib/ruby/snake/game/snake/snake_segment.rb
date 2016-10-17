91dafa3807f8d6b42078cb7d7d6e924d67087335
Patryk Wertka <spartanpageagain@gmail.com>
2016-10-17 09:59:32 +0200

    added zorder and rotation

--- lib/ruby/snake/game/snake/snake_segment.rb
+++ lib/ruby/snake/game/snake/snake_segment.rb
@@ -1,15 +1,24 @@
 require 'ruby/snake/game/entity/angle_based_entity'
-
+require 'gosu'
 module Ruby
   module Snake
     module Game
       module Snake
         # Snake Segment is an angle based entity,
         class SnakeSegment < Game::AngleBasedEntity
-          @@segment_image = Helper::Resource.load('res/snake-head.bmp')
+          @@segment_image = Helper::Resource.load('res/snake-segment.bmp')
+          @@offset = @@segment_image.height / 10
 
           def image
             @@segment_image
+          end
+
+          def zorder
+            0
+          end
+
+          def self.offset
+            @@offset
           end
 
           def draw
@@ -17,7 +26,7 @@ module Ruby
             image.draw_rot(
               @pos_x,
               @pos_y,
-              0, @rotation
+              zorder, @rotation
             )
           end
         end
